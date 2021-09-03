31095a31e4cc25dae599804d57894c717aa36c1c
Rıza Kaan Uçak <rzakaan@gmail.com>
2021-09-03 13:39:33 +0000

    LinkedList implementation

---
+++ index.js
@@ -0,0 +1,9 @@
+const { LinkedList } = require('./lib/linked_list');
+
+// exports.Iterator = Iterator;
+var list = new LinkedList();
+list.add(55);
+list.add(4);
+list.add(10);
+list.add(33);
+list.print()
